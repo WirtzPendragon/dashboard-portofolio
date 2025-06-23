90a3d18d50075895ca3a3c32ec3e3e0dac851cc0
Wirtz Pendragon <adityawiranata2018@gmail.com>
2025-06-23 14:22:48 +0800

    debug

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Project; // Pastikan model Project ada
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -11,13 +11,12 @@ class ProjectController extends Controller
     public function index()
     {
         try {
-            // Ambil semua proyek dari database
             $projects = Project::all();
             
             return response()->json([
                 'success' => true,
                 'message' => 'Data proyek berhasil diambil',
-                'data' => $projects // Kirim data proyek
+                'data' => $projects
             ]);
             
         } catch (\Exception $e) {
